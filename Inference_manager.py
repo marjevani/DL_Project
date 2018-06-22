d808e7123a46a7b48b04ac70b2790ed03dbff0ec
Maor <maor.arjevani@gmail.com>
2018-06-22 11:51:26 +0300

    redisighn for PAINT section

---
+++ Inference_manager.py
@@ -0,0 +1,97 @@
+import paint
+import mnist_object
+from util import *
+import scipy.signal
+import numpy as np
+
+class Inference_manager(object):
+
+    def __init__(self):
+        orig_img = 0
+        processes_img = 0
+        # self.painter = paint.Paint(self)
+        paint.Paint(self)
+
+    def pre_process(self, tile):
+        self.orig_img = tile
+        self.centralize()
+        self.pad()
+        # self.painter.show_img(self.processes_img)
+        return self.processes_img
+
+    def centralize(self):
+        x_pad = []
+        for pad in range(-4, 5):
+            x_cent = 0
+            for row in range(20):
+                for col in range(20):
+                    x_cent = x_cent + ((10 - col + pad) if not self.orig_img[row][col] is None else 0)
+            x_pad.append((pad, x_cent))
+
+        y_pad = []
+        for pad in range(-4, 5):
+            y_cent = 0
+            for row in range(20):
+                for col in range(20):
+                    y_cent = y_cent + ((10 - row + pad) if not self.orig_img[row][col] is None else 0)
+            y_pad.append((pad, y_cent))
+
+        y_pad.sort(key=lambda x: np.abs(x[1]))
+        x_pad.sort(key=lambda x: np.abs(x[1]))
+
+        x_cent, y_cent = x_pad[0][0], y_pad[0][0]
+
+        debug_print(x_cent, y_cent)
+
+        lines_top = [[0] * 20] * (4 - y_cent)
+        lines_bottom = [[0] * 20] * (4 + y_cent)
+
+        lines_left = [[0] * (4 - x_cent)] * 28
+        lines_rigth = [[0] * (4 + x_cent)] * 28
+
+        pic = []
+        for row in self.orig_img:
+            tmp = []
+            pic.append(tmp)
+            for col in row:
+                print_val = "{0:^3}".format(0 if col == None else 255)
+                debug_print(print_val, end=" ")
+                tmp.append(0 if col == None else 255)
+            debug_print()
+
+        pic = lines_top + pic + lines_bottom
+        debug_print(np.array(lines_left).shape, np.array(pic).shape, np.array(lines_rigth).shape)
+        pic = np.concatenate((lines_left, pic, lines_rigth), axis=1)
+
+        # self.painter.show_img(pic)
+        self.processes_img = pic
+
+
+    def pad(self):
+        pic = self.processes_img
+        filter = [
+            [0.01, 0.01, 0.01],
+            [0.01, 1, 0.01],
+            [0.01, 0.01, 0.01]
+        ]
+        debug_print(filter)
+        res = scipy.signal.convolve2d(self.processes_img, filter, mode='same')
+        self.processes_img = res
+        for row in res:
+            for col in row:
+                print_val = "{0:^7.4f}".format(col)
+                debug_print(print_val, end=" ")
+            debug_print()
+
+        # self.painter.show_img(self.processes_img * self.processes_img)
+
+
+    def send_eval(self):
+        mnist_object.eval(self.processes_img)
+
+
+def main():
+    im = Inference_manager()
+
+if __name__ == '__main__':
+    main()
--- paint.py
+++ paint.py
@@ -1,23 +1,23 @@
 from tkinter import *
-import numpy as np
-import scipy.signal
-import mnist_object
-from util import *
+import time
+
 
 ROWS = 20
 COLS = 20
 
 class Paint(object):
 
-    def __init__(self):
+    def __init__(self, im):
+        self.manager = im
+
         ## build GUI ##
         self.root = Tk()
 
-        self.center_btn = Button(self.root, text='center', command=self.centralize)
-        self.center_btn.grid(row=0, column=0)
-
-        self.pad_btn = Button(self.root, text='pad', command=self.pad)
-        self.pad_btn.grid(row=0, column=1)
+        # self.center_btn = Button(self.root, text='center', command=self.centralize)
+        # self.center_btn.grid(row=0, column=0)
+        #
+        # self.pad_btn = Button(self.root, text='pad', command=self.pad)
+        # self.pad_btn.grid(row=0, column=1)
 
         self.send_btn = Button(self.root, text='send', command=self.send_eval)
         self.send_btn.grid(row=0, column=2)
@@ -58,98 +58,31 @@ class Paint(object):
             self.canvas.delete(self.tiles[row][col])
             self.tiles[row][col] = None
 
-    def centralize(self):
-        x_pad =[]
-        for pad in range(-4,5):
-            x_cent = 0
-            for row in range(20):
-                for col in range(20):
-                    x_cent = x_cent + ((10 - col + pad) if not self.tiles[row][col] is None else 0)
-            x_pad.append((pad,x_cent))
 
-        y_pad = []
-        for pad in range(-4, 5):
-            y_cent = 0
-            for row in range(20):
-                for col in range(20):
-                    y_cent = y_cent + ((10 - row + pad) if not self.tiles[row][col] is None else 0)
-            y_pad.append((pad, y_cent))
+    def send_eval(self):
+        processed = self.manager.pre_process(self.tiles)
+        self.show_img(processed*processed)
+        self.manager.send_eval()
 
-        y_pad.sort(key=lambda x : np.abs(x[1]))
-        x_pad.sort(key=lambda x: np.abs(x[1]))
-
-        x_cent,y_cent = x_pad[0][0],y_pad[0][0]
-
-        debug_print(x_cent,y_cent)
-
-        lines_top = [[0] * 20]*  (4-y_cent )
-        lines_bottom = [[0] * 20] * (4+y_cent )
-
-        lines_left = [[0] *(4 - x_cent)] * 28
-        lines_rigth = [[0] * (4 + x_cent)] * 28
-
-        pic = []
-        for row in self.tiles:
-            tmp = []
-            pic.append(tmp)
-            for col in row:
-                print_val = "{0:^3}".format(0 if col == None else 255)
-                debug_print(print_val,end= " ")
-                tmp.append(0 if col == None else 255)
-            debug_print()
-
-
-        col_width = int(self.canvas.winfo_width() / 28)
-        row_height = int(self.canvas.winfo_height() / 28)
-        for row in range(20):
-            for col in range(20):
-                self.canvas.delete(self.tiles[row][col])
-                self.tiles[row][col] = None
-
-        pic = lines_top + pic + lines_bottom
-        debug_print(np.array(lines_left).shape, np.array(pic).shape, np.array(lines_rigth).shape)
-        pic = np.concatenate((lines_left, pic, lines_rigth), axis=1)
-
-        centerd = [[None]*28]*28
-        for row in range(28):
-            for col in range(28):
-                num = int(255- pic[row][col])
-                colorval = "#%02x%02x%02x" % (num,num,num)
-                centerd[row][col] = self.canvas.create_rectangle(col * col_width, row * row_height, (col + 1) * col_width,
-                                                                 (row + 1) * row_height, fill=colorval, outline=colorval)
-        self.pic = pic
-
-
-    def pad(self):
-        pic = self.pic
-        filter = [
-            [0.01, 0.01, 0.01],
-            [0.01, 1, 0.01],
-            [0.01, 0.01, 0.01]
-        ]
-        debug_print(filter)
-        res = scipy.signal.convolve2d(pic, filter, mode='same')
-        self.final = res
-        for row in res:
-            for col in row:
-                print_val = "{0:^7.4f}".format(col)
-                debug_print(print_val, end=" ")
-            debug_print()
-
+    def show_img(self, img):
+        # clear canvas
+        self.canvas.delete("all");
+        # paint img on canvas
         col_width = int(self.canvas.winfo_width() / 28)
         row_height = int(self.canvas.winfo_height() / 28)
 
         centerd = [[None] * 28] * 28
         for row in range(28):
             for col in range(28):
-                num = 255 - int(255 if res[row][col] * res[row][col] > 255 else res[row][col] * res[row][col])
+                num = int(255 - min(img[row][col], 255))
                 colorval = "#%02x%02x%02x" % (num, num, num)
-                centerd[row][col] = self.canvas.create_rectangle(col * col_width, row * row_height, (col + 1) * col_width,
-                                                                 (row + 1) * row_height, fill=colorval, outline=colorval)
+                centerd[row][col] = self.canvas.create_rectangle(col * col_width, row * row_height,
+                                                                 (col + 1) * col_width,
+                                                                 (row + 1) * row_height, fill=colorval,
+                                                                 outline=colorval)
 
-    def send_eval(self):
-        mnist_object.eval(self.final)
 
 
-if __name__ == '__main__':
-    Paint()
+
+# if __name__ == '__main__':
+#     p = Paint()
